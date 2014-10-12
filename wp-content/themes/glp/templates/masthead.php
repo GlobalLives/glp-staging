94da6191f8872acc8ccf2ca5ef88fdb341d3f1f7
Maiya Holliday <maiya@mangrove-web.com>
2014-10-12 13:10:27 -0700

    staging marking

--- wp-content/themes/glp/templates/masthead.php
+++ wp-content/themes/glp/templates/masthead.php
@@ -1,5 +1,5 @@
 <div id="masthead" class="container">
-STAGING
+<div style="position: absolute;top: 10px;">THIS IS THE STAGING SITE</div>
 	<div class="row">
 		<h1 class="site-title col-xs-6"><a class="brand" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a></h1>
 		<div class="site-tabs col-xs-6">
