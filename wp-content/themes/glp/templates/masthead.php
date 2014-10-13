08910e82ff3e570f4304abcad361309e8d374463
Maiya Holliday <maiya@mangrove-web.com>
2014-10-13 12:54:08 -0700

    fixing page container alignment, marking staging

--- wp-content/themes/glp/templates/masthead.php
+++ wp-content/themes/glp/templates/masthead.php
@@ -1,5 +1,5 @@
 <div id="masthead" class="container">
-<div style="position: absolute;top: 10px;">THIS IS THE STAGING SITE</div>
+<div style="position: absolute;top: 10px; background: #eee; color: red; font-weight: bold;">THIS IS THE STAGING SITE</div><!-- MANGROVE REMOVE -->
 	<div class="row">
 		<h1 class="site-title col-xs-6"><a class="brand" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a></h1>
 		<div class="site-tabs col-xs-6">
