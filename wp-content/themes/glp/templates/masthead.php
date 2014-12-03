d5ccece2da7ea801eae4815bc8b319d393e8784b
Kegan Quimby <keganquimby@gmail.com>
2014-12-03 13:56:49 -0800

    fixed sorcetree errors

--- wp-content/themes/glp/templates/masthead.php
+++ wp-content/themes/glp/templates/masthead.php
@@ -1,9 +1,7 @@
 <div id="masthead" class="container">
-<<<<<<< HEAD
+
 <div style="position: absolute;top: 10px; background: #eee; color: red; font-weight: bold;">THIS IS THE STAGING SITE</div><!-- MANGROVE REMOVE -->
-=======
 <div style="position: absolute;top: 10px; color: red; font-weight: bold;">THIS IS THE STAGING SITE</div>
->>>>>>> 7504db0a0c3af20f83ef837c29398635a24d243a
 	<div class="row">
 		<h1 class="site-title col-xs-6"><a class="brand" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a></h1>
 		<div class="site-tabs col-xs-6">
